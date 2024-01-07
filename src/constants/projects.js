8fb899c0773b031068e47af18a759e353f70d5ba
Uttam Makwana <uttamakwana4503@gmail.com>
2024-01-07 20:11:57 +0530

    Updated some content in Projects page and apply some CSS on it.

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -6,53 +6,62 @@ export const projects = [
     time: "September 2023",
     techStack: "React JS, Node, Express, JWT Auth, bcyrpt",
     img: "https://images.unsplash.com/photo-1423666639041-f56000c27a9a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8b25saW5lJTIwc2hvcHBpbmd8ZW58MHx8MHx8fDA%3D",
+    link: "https://github.com/uttamakwana/ecomm/tree/main",
   },
   {
     title: "Splitwise",
     description:
-      "A fully functional full stack web application for online product selling and buying for all categories.",
-    time: "September 2023",
-    techStack: "React JS, Node, Express, JWT Auth, bcyrpt",
+      "Created backend with fully functional APIs with all functionality of add transaction, split transactions, split bills, add friends, send friend request and etc.",
+    techStack: "Express, JWT Auth, bcyrpt",
     img: "https://images.unsplash.com/photo-1518458028785-8fbcd101ebb9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmlsbHN8ZW58MHx8MHx8fDA%3D",
+    link: "https://github.com/uttamakwana/splitwise-backend",
   },
   {
     title: "Eduwise",
     description:
-      "A fully functional full stack web application for online product selling and buying for all categories.",
-    time: "September 2023",
-    techStack: "React JS, Node, Express, JWT Auth, bcyrpt",
+      "One stop Education portal with the tagline 'learn wise with EduWise'. Fully resopnsive with visually appealing UI.",
+    techStack: "React JS",
     img: "https://images.unsplash.com/photo-1491841550275-ad7854e35ca6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZWR1Y2F0aW9ufGVufDB8fDB8fHww",
+    link: "https://eduwisev2.netlify.app",
   },
   {
     title: "Resume Builder",
     description:
-      "A fully functional full stack web application for online product selling and buying for all categories.",
-    time: "September 2023",
+      "Form input resume builder platform to create your ATS friendly resume for best opportunities.",
     techStack: "React JS, Node, Express, JWT Auth, bcyrpt",
     img: "https://images.unsplash.com/photo-1586281380349-632531db7ed4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmVzdW1lfGVufDB8fDB8fHww",
+    link: "https://resumebuilder-qc6l.onrender.com/",
   },
   {
     title: "Yojana Kendra",
     description:
-      "A fully functional full stack web application for online product selling and buying for all categories.",
-    time: "September 2023",
+      "An One Stop Schemes Portal of Government Schemas where you can find all the categories and full details about it with system of notifications and login/register functionality.",
     techStack: "React JS, Node, Express, JWT Auth, bcyrpt",
     img: "https://plus.unsplash.com/premium_photo-1682436975772-d500e798f6f4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c2NoZW1lfGVufDB8fDB8fHwwhttps://images.unsplash.com/photo-1519389950473-47ba0277781c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3R1ZGVudCUyMHBvcnRhbHxlbnwwfHwwfHx8MA%3D%3D",
+    link: "https://github.com/uttamakwana/Yojana-Kendra",
   },
   {
     title: "Student Portal",
     description:
-      "A fully functional full stack web application for online product selling and buying for all categories.",
-    time: "September 2023",
-    techStack: "React JS, Node, Express, JWT Auth, bcyrpt",
+      "Creating a Student Portal for our college, where student can manage its activities and stuff such as Exam Form, Results, Mid Semester Exams, Marks, Personal Documents, Notifications, Alerts, Notices, etc.",
+    techStack: "React JS, Node, Express, JWT Auth, bcyrpt, Mongoose, MongoDB",
     img: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3R1ZGVudCUyMHBvcnRhbHxlbnwwfHwwfHx8MA%3D%3D",
+    link: "",
   },
   {
     title: "JavaScript30",
     description:
-      "A fully functional full stack web application for online product selling and buying for all categories.",
-    time: "September 2023",
-    techStack: "React JS, Node, Express, JWT Auth, bcyrpt",
+      "A course of JavaScript30 with 30 days of learning. I have completed this program to uncover hidden concepts of JavaScript to master it.",
+    techStack: "HTML, CSS, JavaScript",
     img: "https://images.unsplash.com/photo-1627398242454-45a1465c2479?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8amF2YXNjcmlwdHxlbnwwfHwwfHx8MA%3D%3D",
+    link: "https://github.com/uttamakwana/JavaScript30",
+  },
+  {
+    title: "Pomodoro Clone",
+    description:
+      "Created a fully functional Pomodoro Clone with todo list functionality where you can manage your works and study with Pomodoro Technique.",
+    techStack: "React JS",
+    img: "https://images.unsplash.com/photo-1627398242454-45a1465c2479?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8amF2YXNjcmlwdHxlbnwwfHwwfHx8MA%3D%3D",
+    link: "https://uttamakwana.github.io/Pomodoro-Clone/",
   },
 ];
--- src/containers/ProjectCard/ProjectCard.jsx
+++ src/containers/ProjectCard/ProjectCard.jsx
@@ -1,15 +1,19 @@
-import './project-card.css';
+import "./project-card.css";
 
 const ProjectCard = ({ project }) => {
   return (
     <div className="project-card bg-secondary">
       {/* <img src={project.img} alt="project-img" className="project-card-img" /> */}
-      <div className="project-card-content">
+      <a className="project-card-content" href={project.link} target="_blank">
         <h1 className="project-card-title font-extrabold">{project.title}</h1>
-        <p className="project-card-description font-medium">{project.description}</p>
-        <p className="project-card-time font-regular clr-tertiory">{project.time}</p>
+        <p className="project-card-description font-medium">
+          {project.description}
+        </p>
+        <p className="project-card-time font-regular clr-tertiory">
+          {project.time}
+        </p>
         <p className="project-card-techstack font-bold">{project.techStack}</p>
-      </div>
+      </a>
     </div>
   );
 };
